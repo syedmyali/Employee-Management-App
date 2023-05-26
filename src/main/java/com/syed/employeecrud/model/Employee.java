9930d4c2c0ec0e92f868279225274a28d04b8631
SyedAli15 <syounusa@gmail.com>
2023-05-26 13:04:33 +0100

    Commit

---
+++ src/main/java/com/syed/employeecrud/model/Employee.java
@@ -0,0 +1,86 @@
+package com.syed.employeecrud.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employee_table")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "email_id")
+	private String emailId;
+	
+	@Column(name = "phone_No")
+	private Long phoneNo;
+	
+	@Column(name = "department")
+	private String department;
+	
+	public Employee() {
+		
+	}
+	
+	public Employee(String firstName, String lastName, String emailId, Long phoneNo, String department) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+		this.phoneNo = phoneNo;
+		this.department = department;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public Long getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(Long phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+}
---
+++ src/test/java/com/syed/employeecrud/EmployeecrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syed.employeecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeecrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
